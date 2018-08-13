09d60d9054b6e8e841fa8c2d54d5ee78ea388077
Leanid Hreska <Leanid_Hreska@epam.com>
2018-08-13 12:34:40 +0300

    created responsive menu with js

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,4 +1,13 @@
 function handleClickMenu() {
   const menu = document.getElementById('headerMenu');
-  console.log(menu);
+  const menuWrapper = menu.getElementsByClassName('navigation__menu-wrapper')[0];
+  menuWrapper.style.zIndex = 2;
+  menuWrapper.style.opacity = 1;
+}
+
+function handleCloseMenu() {
+  const menu = document.getElementById('headerMenu');
+  const menuWrapper = menu.getElementsByClassName('navigation__menu-wrapper')[0];
+  menuWrapper.style.zIndex = -1;
+  menuWrapper.style.opacity = 0;
 }
